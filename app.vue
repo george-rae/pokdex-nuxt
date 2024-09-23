e6f5e24b069e105373bb03c228c4856cd2c2ead6
George Rae <george.rae@28b.co.uk>
2024-09-23 21:59:04 +0100

    feat(menu): add mobile borgir menu for jumping between gens easily

--- app.vue
+++ app.vue
@@ -3,6 +3,8 @@
 	import Theme from "./components/icons/Theme.vue";
 
 	const { theme } = useAppConfig();
+	theme.dark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
 	const themeClass: ComputedRef<"theme--dark" | "theme--light"> = computed(() =>
 		theme.dark ? "theme--dark" : "theme--light"
 	);
